85e68e6844289ecb28c265ef1070a0ff7981a039
samndambuki <sam.ndambuki08@gmail.com>
2024-01-08 21:50:31 +0300

    worked on the authservice and the AuthGuard

--- src/app/guard/auth.guard.ts
+++ src/app/guard/auth.guard.ts
@@ -1,5 +1,29 @@
-import { CanActivateFn } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { AuthService } from '../service/auth.service';
+import { ToastrService } from 'ngx-toastr';
+import { Observable } from 'rxjs';
 
-export const authGuard: CanActivateFn = (route, state) => {
-  return true;
-};
+
+//define AuthGuard class that implements CanActivate interface
+export class AuthGuard implements CanActivate {
+
+  //costructor takes instances 
+  //parameters
+  constructor(private service: AuthService, private router: Router, private toastr: ToastrService) { }
+
+  //implement canActivate method of CanActivate interface. This method is callled to determine if a route can be activated 
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+    //if the user is logged i nit returns true, allowing navigation to the requested route
+    if (this.service.IsLoggedIn()) {
+      return true
+    } else {
+      //if the user is not loggged in it navigates to the login route using Router Service and returns false
+      this.router.navigate(['login'])
+      return false
+    }
+  }
+
+}
+
+
--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -36,4 +36,24 @@ export class AuthService {
   upDateUser(code: any, inputData: any) {
     return this.http.post(this.apiUrl + '/' + code, inputData)
   }
+
+  IsLoggedIn() {
+    //the function checks if there is a value stored in the username key in the session storage
+    //if the value is not null it implies that there is a user considered logged in and the function returns true otherwise it returns false
+    return sessionStorage.getItem('username') != null;
+  }
+
+  GetUserrole() {
+    //we check if there is a value stored in the username key in the session storage 
+    //if value is not null, we get the value of the username converted to a string using the toString() method 
+    //if value is null we return an empty string
+    return sessionStorage.getItem('username') != null ? sessionStorage.getItem('username')?.toString() : ''
+  }
 }
+
+
+
+/*NAMING CONVECTIONS*/
+//1. camelCase - variable names, method names, function names
+//2. PascalCase - Classes
+//3. SCREAMING_SNAKE_CASE - used for constants      E.G // const MAX_COUNT = 10;
